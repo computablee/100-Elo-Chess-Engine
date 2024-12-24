bad69d49636ffa906cfff155e6b5f35bd9f69784
Phil Lane <phillipallenlane@gmail.com>
2024-12-24 04:06:56 -0800

    improve evaluation function

--- sprt.sh
+++ sprt.sh
@@ -1,4 +1,4 @@
-cutechess-cli -engine cmd=100elo -engine cmd=stash-9.0-linux-x86_64-modern \
+cutechess-cli -engine cmd=100elo-eval -engine cmd=100elo \
     -each tc=10+0.1 proto=uci restart=on -rounds 128000 -concurrency 14 \
     -sprt elo0=0 elo1=10 alpha=0.05 beta=0.05 \
     -openings file=8moves_v3.pgn plies=8
