a416a1f88960a522e1c39da9a578de6d1d62d102
Phil Lane <phillipallenlane@gmail.com>
2025-01-04 05:53:15 -0800

    revert to old eval, new one wasn't faster, erroneous test

--- src/evaluate.cxx
+++ src/evaluate.cxx
@@ -42,20 +42,12 @@ namespace Engine::Evaluate
         int32_t eg[2] = { 0 };
         int phase = 0;
 
-        for (int32_t color = 0; color < 2; color++)
-        {
-            for (int32_t piece = 0; piece < 5; piece++)
-            {
-                auto bb = board.pieces(static_cast<PieceType::underlying>(piece), static_cast<Color::underlying>(color)).getBits();
-
-                while (bb)
-                {
-                    const int lsb = __builtin_ctzll(bb);
-                    mg[color] += pieceSquareTable.get_value_middlegame(color, piece, lsb);
-                    eg[color] += pieceSquareTable.get_value_endgame(color, piece, lsb);
-                    phase += gamePhase[piece];
-                    bb &= bb - 1;
-                }
+        for (uint32_t i = 0; i < 64; i++) {
+            const auto piece = board.at(i);
+            if (piece != Piece::NONE) {
+                mg[static_cast<int>(piece.color())] += pieceSquareTable.get_value_middlegame(piece.color(), piece.type(), i);
+                eg[static_cast<int>(piece.color())] += pieceSquareTable.get_value_endgame(piece.color(), piece.type(), i);
+                phase += gamePhase[static_cast<int>(piece.type())];
             }
         }
 
